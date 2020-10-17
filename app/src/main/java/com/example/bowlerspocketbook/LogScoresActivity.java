14a47e7522b86ad987841252eeeddbad452be8f9
jpre195 <jdpreston7@gmail.com>
2020-10-16 21:32:20 -0400

    Added buttons to main activity.

---
+++ app/src/main/java/com/example/bowlerspocketbook/LogScoresActivity.java
@@ -0,0 +1,14 @@
+package com.example.bowlerspocketbook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LogScoresActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_scores);
+    }
+}
