602749094c76ff4d5326064dc8d4413019b4ac11
jpre195 <jdpreston7@gmail.com>
2020-10-18 14:48:15 -0400

    Added functionality to log scores button and added LogScoresActivity class.

--- app/src/main/java/com/example/bowlerspocketbook/LogScoresActivity.java
+++ app/src/main/java/com/example/bowlerspocketbook/LogScoresActivity.java
@@ -3,12 +3,34 @@ package com.example.bowlerspocketbook;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class LogScoresActivity extends AppCompatActivity {
+public class LogScoresActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_scores);
+
+        Spinner spinner = findViewById(R.id.eventTypeSpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.events_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
--- app/src/main/java/com/example/bowlerspocketbook/MainActivity.java
+++ app/src/main/java/com/example/bowlerspocketbook/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.bowlerspocketbook;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button logScoresBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        logScoresBtn = (Button) findViewById(R.id.logScoresBtn);
+        logScoresBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openLogScoresActivity();
+            }
+        });
+    }
+
+    public void openLogScoresActivity() {
+        Intent intent = new Intent(this, LogScoresActivity.class);
+        startActivity(intent);
     }
 }
