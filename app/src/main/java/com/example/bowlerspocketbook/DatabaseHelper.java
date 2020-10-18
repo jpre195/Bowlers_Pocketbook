f4ee7cc1693eec8ed13d29b27eb3cb5807d7e03b
jpre195 <jdpreston7@gmail.com>
2020-10-18 16:57:47 -0400

    TODO Fix Log Scores button.

---
+++ app/src/main/java/com/example/bowlerspocketbook/DatabaseHelper.java
@@ -0,0 +1,67 @@
+package com.example.bowlerspocketbook;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    //Initialize database
+    private static final String DATABASE_NAME = "bowlers_pocketbook";
+    private static final String TABLE_NAME = "scores";
+
+    DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        //Create table
+        String createTable = "create table " + TABLE_NAME + "(id INTEGER PRIMARY KEY, Event TEXT, Ball TEXT, Score INTEGER, Game INTEGER)";
+        db.execSQL(createTable);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+    public boolean addGame(String event, String ball, int score, int game) {
+        //Get writeable database
+        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
+
+        //Create ContentValues
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("Event", event);
+        contentValues.put("Ball", ball);
+        contentValues.put("Score", score);
+        contentValues.put("Game", game);
+
+        //Add values into database
+        sqLiteDatabase.insert(TABLE_NAME, null, contentValues);
+        return true;
+    }
+
+    public ArrayList getAllScores() {
+        //Get Readable database
+        SQLiteDatabase sqLiteDatabase = this.getReadableDatabase();
+        ArrayList<String> arrayList = new ArrayList<String>();
+
+        //Create cursor to select all values
+        Cursor cursor = sqLiteDatabase.rawQuery("select * from " + TABLE_NAME, null);
+        cursor.moveToFirst();
+
+        while (!cursor.isAfterLast()) {
+            arrayList.add(cursor.getString(cursor.getColumnIndex("Event")));
+            arrayList.add(cursor.getString(cursor.getColumnIndex("Ball")));
+            arrayList.add(cursor.getString(cursor.getColumnIndex("Score")));
+            arrayList.add(cursor.getString(cursor.getColumnIndex("Game")));
+            cursor.moveToNext();
+        }
+
+        return arrayList;
+    }
+}
--- app/src/main/java/com/example/bowlerspocketbook/LogScoresActivity.java
+++ app/src/main/java/com/example/bowlerspocketbook/LogScoresActivity.java
@@ -6,21 +6,84 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class LogScoresActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    //Initialize variables
+    Spinner spinner;
+    EditText ballEditText;
+    EditText scoreEditText;
+    EditText gameEditText;
+    ListView scoreListView;
+    Button logScoresBtn;
+
+    DatabaseHelper databaseHelper;
+    ArrayList arrayList;
+    ArrayAdapter arrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_scores);
 
-        Spinner spinner = findViewById(R.id.eventTypeSpinner);
+        spinner = findViewById(R.id.eventTypeSpinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.events_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
         spinner.setOnItemSelectedListener(this);
+
+        ballEditText = findViewById(R.id.ballUsedEditText);
+        scoreEditText = findViewById(R.id.scoreEditText);
+        gameEditText = findViewById(R.id.gameNumberEditText);
+        scoreListView = findViewById(R.id.scoresListView);
+        logScoresBtn = findViewById(R.id.logScoresBtn);
+
+        //Initialize DatabaseHelper
+        databaseHelper = new DatabaseHelper(LogScoresActivity.this);
+
+        //Add database values to ArrayList
+        arrayList = databaseHelper.getAllScores();
+
+        //Initialize ArrayAdapter
+        arrayAdapter = new ArrayAdapter(LogScoresActivity.this, android.R.layout.simple_list_item_1, arrayList);
+
+        //Set arrayAdapter to ListView
+        scoreListView.setAdapter(arrayAdapter);
+
+        logScoresBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Get values
+                String eventType = spinner.getSelectedItem().toString();
+                String ball = ballEditText.getText().toString();
+                int score = Integer.parseInt(scoreEditText.getText().toString());
+                int game = Integer.parseInt(gameEditText.getText().toString());
+
+                if (!eventType.isEmpty() & !ball.isEmpty() & scoreEditText.getText().toString().isEmpty() & gameEditText.getText().toString().isEmpty()) {
+                    ballEditText.setText("");
+                    scoreEditText.setText("");
+                    gameEditText.setText("");
+
+                    //Display Success message
+                    Toast.makeText(getApplicationContext(), "Score logged...", Toast.LENGTH_SHORT).show();
+
+                    arrayList.clear();
+                    arrayList.addAll(databaseHelper.getAllScores());
+
+                    //Refresh ListView
+                    arrayAdapter.notifyDataSetChanged();
+                    scoreListView.invalidateViews();
+                    scoreListView.refreshDrawableState();
+                }
+            }
+        });
     }
 
     @Override
