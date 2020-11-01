fd0a810a2be9d17ae7686ab5fcde6a593763eda0
jpre195 <jdpreston7@gmail.com>
2020-11-01 16:20:58 -0500

    Latest save.

--- app/src/main/java/com/example/bowlerspocketbook/AnalyzeScoresActivity.java
+++ app/src/main/java/com/example/bowlerspocketbook/AnalyzeScoresActivity.java
@@ -7,10 +7,18 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.View;
 
 public class AnalyzeScoresActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    RecyclerView.Adapter programAdapter;
+    RecyclerView.LayoutManager layoutManager;
+
+    //TODO add a test scores list, event type list, and game number list (at 9:33 in video)
+    String[] scoresList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
