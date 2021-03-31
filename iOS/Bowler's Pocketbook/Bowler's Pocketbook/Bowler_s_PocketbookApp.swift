985ed3bedb97e9fdc24d3938fa7ddcf45a687c50
Jeffrey Preston <jeffreypreston@macbook-air.myfiosgateway.com>
2021-03-31 06:38:42 -0400

    Started iOS app

---
+++ iOS/Bowler's Pocketbook/Bowler's Pocketbook/Bowler_s_PocketbookApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bowler_s_PocketbookApp.swift
+//  Bowler's Pocketbook
+//
+//  Created by Jeffrey Preston on 3/31/21.
+//
+
+import SwiftUI
+
+@main
+struct Bowler_s_PocketbookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
