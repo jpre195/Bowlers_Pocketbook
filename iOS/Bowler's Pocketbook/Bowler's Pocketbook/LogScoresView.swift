9ca9c3b78f33fadd9ee35086e4c5adefab9ccc3a
Jeff Preston <jdpreston7@gmail.com>
2021-04-25 21:23:35 -0400

    Started LogScoresView using forms

--- iOS/Bowler's Pocketbook/Bowler's Pocketbook/LogScoresView.swift
+++ iOS/Bowler's Pocketbook/Bowler's Pocketbook/LogScoresView.swift
@@ -9,13 +9,86 @@ import SwiftUI
 
 struct LogScoresView: View {
     
+    let eventTypes: [String] = ["Practice", "League", "Tournament"]
+    
+    @State var selectedEvent = "Practice"
+    @State var score = ""
+    @State var gameDate = Date()
+    
     var body: some View {
 
-        VStack {
-            HStack {
-                Text("Log Scores").bold().font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-            }
-            Spacer()
+//        VStack {
+//
+//            HStack {
+//
+//                Text("Select event type:").padding(10)
+//
+//                Spacer()
+//
+//                Picker("Event type",
+//                       selection: $selectedEvent) {
+//                                ForEach(eventTypes, id: \.self) {
+//                                    Text($0)
+//                                }
+//                }.pickerStyle(MenuPickerStyle()).padding(20)
+//
+//
+//            }.padding(.top, 20)
+//
+//            HStack {
+//
+//                Text("Ball used:").padding(10)
+//
+//                Spacer()
+//
+//                Text("You selected: \(selectedEvent)").padding(10)
+//
+//            }
+//
+//            HStack {
+//
+//                Text("Score:").padding(10)
+//
+//                Spacer()
+//
+//            }
+//
+//            HStack {
+//
+//                Text("Date:").padding(10)
+//
+//                Spacer()
+//
+//            }
+//
+//            Spacer()
+//        }
+        
+        NavigationView {
+            
+            Form {
+                
+                Picker(selection: $selectedEvent, label: Text("Event Type:")) {
+                    ForEach(eventTypes, id: \.self) {
+
+                        Text($0)
+                        
+                    }
+                        
+                }
+                    
+                Text("Ball used:")
+                
+                TextField("Score", text: $score)
+                    .keyboardType(.numberPad)
+                
+                DatePicker(selection: $gameDate,
+                           displayedComponents: .date,
+                           label: { Text("Date:") })
+
+                
+            }.navigationBarTitle("Log Scores")
+        
         }
         
     }
